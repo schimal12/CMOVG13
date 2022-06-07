36b79abae6fcb10cac4f5003974cea31128f5807
maria <mariaurena@correo.ugr.es>
2022-06-07 09:16:14 +0200

    camera is okey, starting with map...

--- app/src/main/java/pt/ulisboa/tecnico/cmov/cmovg13/ChatRoom.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovg13/ChatRoom.java
@@ -1,15 +1,27 @@
 package pt.ulisboa.tecnico.cmov.cmovg13;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.service.autofill.ImageTransformation;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,9 +37,16 @@ import com.github.nkzawa.socketio.client.Socket;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import pt.ulisboa.tecnico.cmov.cmovg13.model.Message;
 
@@ -43,8 +62,14 @@ public class ChatRoom extends AppCompatActivity {
 
     // attach a camera image
     // define the button and imageview type variable
-    ImageButton camera_open_id;
-    ImageView click_image_id;
+    ImageButton camera_open_id; // take the photo
+    // show the photo
+    private ImageView mPhotoImageView;
+    private String photoURI;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    // google maps
+    ImageButton maps_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +88,6 @@ public class ChatRoom extends AppCompatActivity {
         // by ID we can get each component which id is assigned in XML file
         // get Buttons and ImageView
         camera_open_id = (ImageButton) findViewById(R.id.camera);
-        click_image_id = (ImageView) findViewById(R.id.imageView);
 
         // camera_open is for open the camera and add the setOnCLickListener in this button
         camera_open_id.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +97,20 @@ public class ChatRoom extends AppCompatActivity {
                 startActivity(camera_intent);
             }
         });
+
+        // maps
+        maps_button = (ImageButton) findViewById(R.id.map);
+        maps_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent maps_intent = new Intent(ChatRoom.this,MapsActivity.class);
+                startActivity(maps_intent);
+            }
+        });
+
+        // to show the photo
+        mPhotoImageView = findViewById(R.id.imageView);
+        //mPhotoImageView.setOnClickListener((View.OnClickListener) this);
 
         // MAPS
         // Set the layout content view
@@ -163,18 +201,49 @@ public class ChatRoom extends AppCompatActivity {
                 }
             }
         });
+        camera_open_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    abrirCamara();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
+
+    private void abrirCamara() throws  IOException{
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File imagenArchivo = null;
+
+        try{
+            imagenArchivo = createImageFile();
+
+        }catch (IOException ex){
+            Log.e("Error", ex.toString());
+        }
+
+
+        if(imagenArchivo != null)
+        {
+            Uri fotoUri = FileProvider.getUriForFile(this, "com.example.myapplication.fileprovider", imagenArchivo);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+        }
+
+        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+    }
+
 
     // save the image
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // BitMap is data structure of image file which store the image in memory
-        Bitmap photo = (Bitmap) data.getExtras().get("data");
-
-        // Set the image in imageview for display
-        click_image_id.setImageBitmap(photo);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bitmap imgBitMap = BitmapFactory.decodeFile(photoURI);
+            mPhotoImageView.setImageBitmap(imgBitMap);
+        }
     }
 
     /*
@@ -185,11 +254,22 @@ public class ChatRoom extends AppCompatActivity {
     }
      */
 
+    private File createImageFile() throws  IOException{
+        String nombreImagen = "foto_";
+        File directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imagen = File.createTempFile(nombreImagen, ".jpg", directorio);
+
+        photoURI = imagen.getAbsolutePath();
+        Log.d("PHOTOURI:",photoURI);
+        return imagen;
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         socket.disconnect();
     }
+
 
 }
 
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovg13/MapsActivity.java
@@ -0,0 +1,68 @@
+package pt.ulisboa.tecnico.cmov.cmovg13;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import pt.ulisboa.tecnico.cmov.cmovg13.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        getLocalizacion();
+    }
+
+    private void getLocalizacion() {
+        int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (permiso == PackageManager.PERMISSION_DENIED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
