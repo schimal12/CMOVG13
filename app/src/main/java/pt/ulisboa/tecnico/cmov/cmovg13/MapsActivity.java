158c5c49193c8d961e0d5af77e3cea7e1cc094b5
maria <mariaurena@correo.ugr.es>
2022-06-09 17:25:18 +0200

    prueba1

--- app/src/main/java/pt/ulisboa/tecnico/cmov/cmovg13/MapsActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/cmovg13/MapsActivity.java
@@ -3,17 +3,23 @@ package pt.ulisboa.tecnico.cmov.cmovg13;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,15 +28,23 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    ImageView imageViewSearch;
+    EditText inputLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_maps);
+        imageViewSearch = findViewById(R.id.imageViewSearch);
+        inputLocation = findViewById(R.id.inputLocation);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -38,13 +52,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         getLocalizacion();
+
+        imageViewSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = inputLocation.getText().toString();
+                if (location == null){
+                    Toast.makeText(MapsActivity.this,"Type any location name",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+                    try {
+                        List<Address> listAddress = geocoder.getFromLocationName(location,1);
+                        if (listAddress.size()>0){
+                            LatLng latLng = new LatLng(listAddress.get(0).getLatitude(),listAddress.get(0).getLongitude());
+                            MarkerOptions markerOptions = new MarkerOptions();
+                            markerOptions.title("My search location");
+                            markerOptions.position(latLng);
+                            mMap.addMarker(markerOptions);
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,5);
+                            mMap.animateCamera(cameraUpdate);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     private void getLocalizacion() {
-        Log.e("2", "dentro de getLocalizacion");
         int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
         if (permiso == PackageManager.PERMISSION_DENIED){
-            Log.e("3", "permiso denegado");
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
 
             }else{
@@ -66,18 +105,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.e("1","entrando en OnMapReady...");
         mMap = googleMap;
 
         if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-            Log.e("2","primer if ");
             return;
         }
 
         mMap.setMyLocationEnabled(true);
-        Log.e("3","setMyLocationEnabled(true)");
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        Log.e("4","setMyLocationButtonEnabled(false)");
 
 
         LocationManager locationManager = (LocationManager) MapsActivity.this.getSystemService(Context.LOCATION_SERVICE);
@@ -107,5 +142,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         };
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+
     }
 }
